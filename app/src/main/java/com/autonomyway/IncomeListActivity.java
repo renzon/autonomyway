c80e737338e83003a1690eea72e2536363c87256
renzon <renzon@gmail.com>
2017-04-01 23:47:06 -0300

    First Income form draft done

--- app/src/main/java/com/autonomyway/IncomeListActivity.java
+++ app/src/main/java/com/autonomyway/IncomeListActivity.java
@@ -1,9 +1,8 @@
 package com.autonomyway;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -31,11 +30,12 @@ public class IncomeListActivity extends ActivityWithFacadeAccess {
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final IncomeListActivity that = this;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(that, IncomeFormActivity.class);
+                startActivity(intent);
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
