8ac3cb036f752244ec41e03aed08ab6c99ba5227
renzon <renzon@gmail.com>
2017-04-06 00:17:57 -0300

    Refactored BaseListAdapter
    Intention of applying into IncomeListAdapter as well

---
+++ app/src/main/java/com/autonomyway/component/base/BaseListAdapter.java
@@ -0,0 +1,54 @@
+package com.autonomyway.component.base;
+
+import android.content.res.Resources;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.autonomyway.model.Model;
+
+import java.util.List;
+
+
+public abstract class BaseListAdapter<M extends Model, BR extends BaseRow<M>,
+        BLVH extends BaseListViewHolder<M, BR>>
+        extends RecyclerView.Adapter<BLVH> {
+
+    List<M> dataSet;
+    private final Resources resources;
+
+    public BaseListAdapter(List<M> dataSet, Resources resources) {
+        this.dataSet = dataSet;
+        this.resources = resources;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public BLVH onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        BR baseRow = (BR) LayoutInflater.from(parent.getContext())
+                .inflate(getRowLayoutId(), parent, false);
+        baseRow.setResources(resources);
+        // set the view's size, margins, paddings and layout parameters
+        BLVH vh = createViewHolder(baseRow);
+        return vh;
+    }
+
+    protected abstract BLVH createViewHolder(BR baseRow);
+
+    protected abstract int getRowLayoutId();
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(BLVH holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.populate(dataSet, position);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return dataSet.size();
+    }
+}
--- app/src/main/java/com/autonomyway/component/wealth/WealthListAdapter.java
+++ app/src/main/java/com/autonomyway/component/wealth/WealthListAdapter.java
@@ -1,49 +1,27 @@
 package com.autonomyway.component.wealth;
 
 import android.content.res.Resources;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
 
 import com.autonomyway.R;
+import com.autonomyway.component.base.BaseListAdapter;
 import com.autonomyway.model.Wealth;
 
 import java.util.List;
 
 
-public class WealthListAdapter extends RecyclerView.Adapter<WealthListViewHolder> {
-    List<Wealth> dataSet;
-    private final Resources resources;
-
+public class WealthListAdapter extends BaseListAdapter<Wealth, WealthRow, WealthListViewHolder> {
     public WealthListAdapter(List<Wealth> dataSet, Resources resources) {
-        this.dataSet = dataSet;
-        this.resources = resources;
+        super(dataSet, resources);
     }
 
-    // Create new views (invoked by the layout manager)
     @Override
-    public WealthListViewHolder onCreateViewHolder(ViewGroup parent,
-                                                   int viewType) {
-        // create a new view
-        WealthRow v = (WealthRow) LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.wealth_row, parent, false);
-        v.setResources(resources);
-        // set the view's size, margins, paddings and layout parameters
-        WealthListViewHolder vh = new WealthListViewHolder(v);
-        return vh;
+    protected WealthListViewHolder createViewHolder(WealthRow wealthRow) {
+        return new WealthListViewHolder(wealthRow);
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(WealthListViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        holder.populate(dataSet, position);
+    protected int getRowLayoutId() {
+        return R.layout.wealth_row;
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
-    @Override
-    public int getItemCount() {
-        return dataSet.size();
-    }
 }
