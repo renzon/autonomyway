b94bae33701d88c244cc7d1611e5a1ce549b8342
renzon <renzon@gmail.com>
2017-04-07 18:47:57 -0300

    Created DateInput

---
+++ app/src/main/java/com/autonomyway/component/common/DateInput.java
@@ -0,0 +1,112 @@
+package com.autonomyway.component.common;
+
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.autonomyway.R;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
+public class DateInput extends LinearLayout implements DatePickerDialog.OnDateSetListener {
+    private final TextView dateView;
+    private Date date;
+    private FragmentManager supportFragmentManager;
+
+
+    public DateInput(Context context) {
+        this(context, null);
+    }
+
+    public DateInput(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DateInput(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public DateInput(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        inflate(getContext(), R.layout.date_input, this);
+        dateView = (TextView) findViewById(R.id.date_view);
+        setDate(new Date());
+        final DateInput that = this;
+        findViewById(R.id.change_date_button).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                c.setTime(that.getDate());
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                final DatePickerDialog dpd = new DatePickerDialog(getContext(), that, year, month,
+                        day);
+                DatePickerFragment datePickerFragment = new DatePickerFragment(dpd);
+                datePickerFragment.show(that.getSupportFragmentManager(),"datePicker");
+            }
+        });
+
+    }
+
+    public void setDate(Date date) {
+        DateFormat fmt = DateFormat.getDateInstance(DateFormat.SHORT);
+        this.date = date;
+        dateView.setText(fmt.format(date));
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c =Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        setDate(c.getTime());
+
+    }
+
+    private FragmentManager getSupportFragmentManager() {
+        return supportFragmentManager;
+    }
+
+    public void setSupportFragmentManager(FragmentManager supportFragmentManager) {
+        this.supportFragmentManager = supportFragmentManager;
+    }
+
+
+
+    public static final class DatePickerFragment extends DialogFragment {
+        private DatePickerDialog datePickerDialog;
+
+        public DatePickerFragment() {
+        }
+        @SuppressLint("ValidFragment")
+        public DatePickerFragment(DatePickerDialog datePickerDialog) {
+            this.datePickerDialog = datePickerDialog;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return datePickerDialog;
+        }
+
+
+    }
+}
--- app/src/main/java/com/autonomyway/component/transfer/NewTransferActivity.java
+++ app/src/main/java/com/autonomyway/component/transfer/NewTransferActivity.java
@@ -1,17 +1,29 @@
 package com.autonomyway.component.transfer;
 
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 
 import com.autonomyway.R;
+import com.autonomyway.component.common.DateInput;
 import com.autonomyway.model.Nameable;
 
 import java.util.Date;
 
 public class NewTransferActivity extends BaseTransferActivity {
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        ((DateInput)findViewById(R.id.date_input)).
+                setSupportFragmentManager(supportFragmentManager);
+    }
+
+    @Override
     protected void saveTransfer(View v, Nameable origin, Nameable destination, Date date, long
             cash, long duration, String detail) {
         autonomy.createTransfer(origin, destination, date, cash, duration, detail);
         showMsg(v, R.string.transfer_save_msg);
+
     }
 }
